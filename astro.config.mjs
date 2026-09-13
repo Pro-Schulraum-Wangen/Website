f9348ed5e23c2ba58ac08dec4c89bc0a05f5f46d
philippbruhin <info@philippbruhin.ch>
2026-09-13 11:07:38 +0200

    update button styles and add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pro-schulraum-wangen.ch',
+	integrations: [sitemap()],
 });
