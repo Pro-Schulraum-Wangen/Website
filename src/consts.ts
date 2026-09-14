ca28ad1ad71a2b6f03c0578482839f0109b3c9ed
philippbruhin <info@philippbruhin.ch>
2026-09-15 00:08:42 +0200

    restructured homepage with less details

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 export const SITE_TITLE = 'Pro Schulraum Wangen';
-export const SITE_TAGLINE =
-	'Unabhängige Unterstützer:innengruppe für die neue Schul- und Sportanlage «Am Buechberg»';
+export const SITE_TAGLINE = 'Weil uns Wangen am Herzen liegt';
 export const SITE_DESCRIPTION =
 	'Wir sind Einwohnerinnen und Einwohner von Wangen SZ, die sich für ein Ja zum Neubau der Schulanlage «Am Buechberg» einsetzen. Argumente, Videos und Unterstützer:innen zur Abstimmung vom 29. November 2026.';
 
@@ -73,6 +72,7 @@ export const VOTE_DATE_LABEL = 'Urnenabstimmung: Sonntag, 29. November 2026';
 export const NAV_LINKS = [
 	{ href: '/', label: 'Start' },
 	{ href: '/argumente/', label: 'Argumente' },
+	{ href: '/fakten/', label: 'Fakten' },
 	{ href: '/unterstuetzer/', label: 'Unterstützer:innen' },
 	{ href: '/videos/', label: 'Videos' },
 	{ href: '/mitmachen/', label: 'Mitmachen' },
